bfcdb6233c13139d18a4eb0955525c9c29b81afe
Mariam Ezzat <85201077+MariamEzzat1@users.noreply.github.com>
2022-09-10 23:19:14 +0200

    Add files via upload

--- ctrl_pkg/src/frontSteering_model.py
+++ ctrl_pkg/src/frontSteering_model.py
@@ -6,9 +6,12 @@ from turtlesim.msg import Pose
 from math import atan,cos,sin,pi,tan
 
 '''from yaml'''
-Lr=2 #rospy.get_param("/Lr")
-Lf=5#rospy.get_param("/Lf")
-
+Lr=rospy.get_param("/Lr")
+Lf=rospy.get_param("/Lf")
+"-----------Inputs and arguments preparation------------"
+lin_vel=float(input("Enter the car velocity\n") )#where lin_vel=0.5r(wl+wr) as the radius of each wheel is the same 
+delta= float(input("Enter the steering angle: \n"))
+sim_time=float(input("And the time of simulation"))
 '''----------------------------------subscriber--------------------------------------'''
 '''once the subscriber get a new data save it in current position'''
 current_pose=Pose()
@@ -24,17 +27,18 @@ def listener():
     pose_listener=rospy.Subscriber("/turtle1/pose",Pose,callback_pose)
 '''----------------------------------publisher------------------------------------------'''
 '''publisher node which publishes the linear and angle velociety to turtle1/cmd_vel topic'''
-def publishing(beta,delta,sim_time):
+def publishing():
+ 
+    beta=atan((Lr/(Lf+Lr))*tan(delta))
     start_time=time.time()
     vel_publisher=rospy.Publisher("/turtle1/cmd_vel",Twist,queue_size=10)
-    '''h publish lw l goal bta3y ab3d mn mkan mo3yn so ana mhtaga min dis lets se it 0.5'''
     while (1):
         msg=Twist()
-        msg.linear.x,msg.linear.y=linear_velociety(beta)
+        msg.linear.x,msg.linear.y=linear_velociety(beta,lin_vel)
         msg.linear.z=0
         msg.angular.x=0
         msg.angular.y=0
-        msg.angular.z=angular_velociety(beta,delta)
+        msg.angular.z=angular_velociety(beta,delta,lin_vel)
         vel_publisher.publish(msg)
         if(time.time()-start_time-sim_time>0.2):
            break
@@ -42,29 +46,22 @@ def publishing(beta,delta,sim_time):
 
 
 '''function to calculate the X and Y components of the linear velocity''' 
-def linear_velociety (beta):
+def linear_velociety (beta,lin_vel):
     Vx=lin_vel*cos(beta+current_pose.theta)
     Vy=lin_vel *sin(beta+current_pose.theta)
     return Vx,Vy
 
     
 '''----------------------fuction to calc the angular velocity-------------------------------------'''
-def angular_velociety (beta,delta):
+def angular_velociety (beta,delta,lin_vel):
     ang =(lin_vel*cos(beta)*tan(delta))/(Lf+Lr)
     return ang
 
 
-'''--------------------------------------main function i------------------------------------------ '''
+'''--------------------------------------main function ------------------------------------------ '''
 if __name__=="__main__":
     try:
-        "-----------Inputs and arguments preparation------------"
-        lin_vel=float(input("Enter the car velocity\n") )#where lin_vel=0.5r(wl+wr) as the radius of each wheel is the same 
-        delta= float(input("Enter the steering angle: \n"))
-        time_of_simulation=float(input("And the time of simulation"))
-        beta=atan((Lr/(Lf+Lr))*tan(delta))
-        
-
         listener()
-        publishing(beta,delta,time_of_simulation)
+        publishing()
     except rospy.ROSInterruptException:
         pass
